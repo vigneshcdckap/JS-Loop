d966f74c47dd7518093de6fbdd6b1721198f8b0c
DCKAP <vigneshcdckap@gmail.com>
2023-10-13 16:44:56 +0530

    Loop

---
+++ main.js
@@ -0,0 +1,39 @@
+let arr=[1,2,3,4,5,6,7,8,9,10];
+let arr2=[];
+for (i =0 ;i<=arr.length-1;i++)
+{
+    arr2[i]=arr[i]*2
+}
+console.log(arr2);
+
+// ----------------- for loops ---------------
+
+
+let ar = [1,2,3,4,5,6,7,8]
+
+let ar1 = [];
+console.log("hello")
+console.log("hello")
+console.log("hello")
+console.log("hello")
+console.log("hello")
+console.log("hello")
+console.log("hello")
+
+
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[2])
+
+console.log(ar)
+for(i=0;i<=ar.length-1;i++)
+{
+  ar1[i] = ar[i] * 2;
+}
+
+
+console.log(ar1)
+
+for(i=0;i<=10;i++){
+  console.log(i)
+}
